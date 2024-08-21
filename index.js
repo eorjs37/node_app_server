901240f9677399de3c876b8dc157f82e1ccb162d
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-08-22 08:03:21 +0900

    더미 api 추가

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // 기본 설정
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 
 // 정적 파일 불러오기
 app.use(express.static(__dirname + "/public"));
 
+app.use(cors({
+    origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
+}));
 
 // 라우팅 정의
 app.get("/", (req, res) => {
@@ -14,6 +18,8 @@ app.get("/", (req, res) => {
 
 //더미 api
 app.get("/api/dummy",(_,res)=>{
+	console.log("call /api/dummy");
+
 	res.status(200).json({
 		"message":"TEST"
 	})
