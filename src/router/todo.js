6bb98d917190fbd5423f0a2108b81137ba4b05a2
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-09-10 08:07:56 +0900

    todo save 추가

--- src/router/todo.js
+++ src/router/todo.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database/mariadb')
+const pool = require('../database/mariadb');
 router.post("/",(_,res) =>{
 	pool.getConnection((err,conn)=>{
 		if(err) return;
-
-		conn.query("INSERT INTO ToDo(todo) VALUES('할일2')",(queryErr,callback)=>{
-			if(queryErr) throw queryErr;
-			res.status(200)
-			.json({
-				success:true
-			})
+		conn.query("INSERT INTO ToDo(todo) VALUES('할일4')",(queryErr,result)=>{
+			if(!queryErr){
+				res.status(200)
+				.json({
+					success:true,
+					result
+				})
+			}else{
+				throw queryErr
+			}
 		})
-		if (conn) return conn.release();
+	})
+})
+
+router.get("/",(_,res)=>{
+	pool.getConnection((err,conn)=>{
+		if(err) return;
+		conn.query("SELECT * FROM ToDo",(queryErr,result)=>{
+			if(!queryErr){
+				res.status(200)
+				.json({
+					success:true,
+					result:{
+						todolist:result
+					}
+				})
+
+				if (conn) return conn.release();
+			}else{
+				throw queryErr
+			}
+		})
+
+
 	})
 })
 
